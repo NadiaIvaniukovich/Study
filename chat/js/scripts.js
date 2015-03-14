3cfaf19e1eec522546327464a47be6132258e4d9
IvaniukovichNadya <hopeful.nadya@gmail.com>
2015-03-14 14:45:52 +0300

    Chat. Хранение данных. Состояние. JSON.

--- chat/js/scripts.js
+++ chat/js/scripts.js
@@ -1,98 +1,227 @@
+var uniqueId = function() {
+	var date = Date.now();
+	var random = Math.random() * Math.random();
+
+	return Math.floor(date * random).toString();
+};
+
+var nickname;
+
+var theMsg = function(name, text) {
+	return {
+		name:nickname,
+		description:text,
+		id: uniqueId()
+	};
+};
+
+var msgList = [];
+
 function run(){
 	var appContainer = document.getElementsByClassName('chat')[0];
 	
 	appContainer.addEventListener('click', delegateEvent);
+	
+	nickname = restoreName()||"Гость";
+	setCurrentNickname();
+	var allMsgs = restoreHistory();
+	createAllMsgs(allMsgs);
+	
+}
+
+function createAllMsgs(allMsgs) {
+	for(var i = 0; i < allMsgs.length; i++)
+		addMsg(allMsgs[i]);
+}
+
+function setCurrentNickname(){
+	var name = document.getElementsByClassName('nickname')[0];
+	if(nickname == ''){
+		return;
+	}
+	name.innerText = nickname;
+}
+
+function addMsg(msg) {
+	var item = createItem(msg);
+	var items = document.getElementById('msgHis');
+
+	msgList.push(msg);
+	items.appendChild(item);
+}
+
+function createItem(msg){
+	var temp = document.createElement('div');
+	var htmlAsText = '<li data-task-id="идентификатор">'+
+	'<input type="button" value="Редактировать" class="button15 edit-btn"><input type="button" value="Удалить" class="button15 delete-btn"><span>имя</span><span>: </span>текст сообщения<br></li>';
+
+	temp.innerHTML = htmlAsText;
+	updateItem(temp.firstChild, msg);
+
+	return temp.firstChild;
+}
+
+function updateItem(liItem, msg){
+	
+	liItem.setAttribute('data-task-id', msg.id);
+	liItem.childNodes[2].textContent = msg.name;
+	liItem.childNodes[4].textContent = msg.description;
 }
 
 function delegateEvent(evtObj){
 	if(evtObj.type === 'click' && evtObj.target.classList.contains('change-name')){
-		changeName(evtObj);
+		onChangeNameButtonClick();
 	} else if(evtObj.type === 'click' && evtObj.target.classList.contains('send-msg')){
-		sendMessage(evtObj);
+		onSendButtonClick();
 	} else if(evtObj.type === 'click' && evtObj.target.classList.contains('delete-btn')){
 		var labelEl = evtObj.target.parentElement;
-		deleteMessage(labelEl);
+		if(labelEl.childNodes[2].textContent == nickname){
+			deleteMessage(labelEl);
+		}
 	} else if(evtObj.type === 'click' && evtObj.target.classList.contains('edit-btn')){
 		var labelEl = evtObj.target.parentElement;
-		editMessage(labelEl);
+		if(labelEl.childNodes[2].textContent == nickname){
+			changeTextColor(labelEl);
+			onEditButtonClick(labelEl);
+		}
 	}else if(evtObj.type === 'click' && evtObj.target.classList.contains('change-msg')){
-		changeText();
+		onChangeButtonClick();
 	}
 }
 
-var nickname = "Гость";
-
-function changeName(evtObj){
+function onChangeNameButtonClick(){
 	var nameText = document.getElementById('name-input');
-	var name = document.getElementsByClassName('nickname')[0];
-	if(nameText.value != ''){
-		nickname = nameText.value;
-		name.innerText = nickname;
+	
+	if(nameText.value == ''){
+		return;
 	}
+	nickname = nameText.value;
+	setCurrentNickname();
+	storeName(nickname);
 	nameText.value = '';
 }
+	
+function onSendButtonClick(){
+	msgText = document.getElementsByClassName('msg-input')[0];
+	var newMsg = theMsg(nickname, msgText.value);
+
+	if(msgText.value == '')
+		return;
+
+	addMsg(newMsg);
+	msgText.value = '';
+	storeHistory(msgList);
+} 
 
 var msgText;
+var htmlItem;
+var sendBtn;
 
-function sendMessage(evtObj){
+function onEditButtonClick(liItem) {	
+	htmlItem = liItem;	
+	var textNode = htmlItem.childNodes[4];
 	msgText = document.getElementsByClassName('msg-input')[0];
-	addMessage(msgText.value);
-	msgText.value = '';	
+	msgText.value = textNode.textContent;
+	sendBtn = document.getElementsByClassName('send-msg')[0];
+	if(sendBtn != null){
+		changeButton(true);
+	}
 }
 
-function addMessage(value) {
-	if(!value){
+function changeTextColor(liItem){
+	var item = document.getElementsByClassName('edit-text')[0];
+	if(item){
+		item.classList.remove('edit-text');
+	}
+	
+	liItem.classList.add('edit-text');
+}
+
+function onChangeButtonClick(){
+	var id = htmlItem.attributes['data-task-id'].value;
+	for(var i = 0; i < msgList.length; i++) {
+		if(msgList[i].id != id)
+			continue;
+
+		changeMsgText(msgList[i]);
+		updateItem(htmlItem, msgList[i]);
+		storeHistory(msgList);
+		htmlItem.classList.remove('edit-text');
+		sendBtn = document.getElementsByClassName('change-msg')[0];
+		changeButton(false);
+		msgText.value = '';
+
+		return;
+	}	
+}
+
+function changeMsgText(msg){
+	msg.description = msgText.value;
+}
+
+function changeButton(flag){
+	if(flag){
+		sendBtn.classList.remove('send-msg');
+		sendBtn.classList.add('change-msg');
+	}else{
+		sendBtn.classList.remove('change-msg');
+		sendBtn.classList.add('send-msg');
+	}
+}
+
+function deleteMessage(liItem){
+	var id = liItem.attributes['data-task-id'].value;
+	
+	var items = document.getElementById('msgHis');
+	
+	for(var i = 0; i < msgList.length; i++) {
+		if(msgList[i].id != id)
+			continue;
+
+		msgList.splice(i,1);
+		storeHistory(msgList);
+		items.removeChild(liItem);
+		return;
+	}
+}
+
+function storeHistory(listToSave) {
+
+	if(typeof(Storage) == "undefined") {
+		alert('localStorage is not accessible');
 		return;
 	}
 
-	var item = createItem(value);
-	var items = document.getElementById('msgHis');
+	localStorage.setItem("Msg history", JSON.stringify(listToSave));
+}
+function storeName(name) {
 
-	items.appendChild(item);
+	if(typeof(Storage) == "undefined") {
+		alert('localStorage is not accessible');
+		return;
+	}
+
+	localStorage.setItem("Nickname", JSON.stringify(name));
 }
 
-function createItem(text){
-	var liItem = document.createElement('li');
-	var deleteButton = document.createElement('input');
-	var editButton = document.createElement('input');
-	
-	deleteButton.setAttribute('type', 'button');
-	deleteButton.setAttribute('value', 'Удалить');
-	deleteButton.classList.add('button15', 'delete-btn');
-	
-	editButton.setAttribute('type', 'button');
-	editButton.setAttribute('value', 'Редактировать');
-	editButton.classList.add('button15', 'edit-btn')
+function restoreHistory() {
+	if(typeof(Storage) == "undefined") {
+		alert('localStorage is not accessible');
+		return;
+	}
 
-	liItem.appendChild(editButton);
-	liItem.appendChild(deleteButton);
-	liItem.appendChild(document.createTextNode(nickname+": "))
-	liItem.appendChild(document.createTextNode(text));
-	liItem.appendChild(document.createElement('br'));	
+	var item = localStorage.getItem("Msg history");
 
-	return liItem;
+	return item && JSON.parse(item);
 }
 
-function deleteMessage(labelEl){
-	var items = document.getElementById('msgHis');
-	items.removeChild(labelEl);
-}
+function restoreName() {
+	if(typeof(Storage) == "undefined") {
+		alert('localStorage is not accessible');
+		return;
+	}
 
-var textNode;
+	var item = localStorage.getItem("Nickname");
 
-function editMessage(labelEl){
-	textNode = labelEl.childNodes[3];
-	msgText = document.getElementsByClassName('msg-input')[0];
-	msgText.value = textNode.textContent;
-	var sendBtn = document.getElementsByClassName('send-msg')[0];
-	sendBtn.classList.remove('send-msg');
-	sendBtn.classList.add('change-msg');
-}
-
-function changeText(){
-	textNode.textContent = msgText.value;
-	var sendBtn = document.getElementsByClassName('change-msg')[0];
-	sendBtn.classList.remove('change-msg');
-	sendBtn.classList.add('send-msg');
-	msgText.value = '';
+	return item && JSON.parse(item);
 }
